2ed10b1298cd1c36300668e27582c6797d2986c2
annaberglind <annaj11@ru.is>
2014-10-03 20:38:32 +0000

    projectextensions

---
+++ CoursesAPI.Services/Extensions/ProjectExtensions.cs
@@ -0,0 +1,23 @@
+﻿using CoursesAPI.Services.DataAccess;
+using CoursesAPI.Services.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursesAPI.Services.Extensions
+{
+    public static class ProjectExtensions
+    {
+        public static Project GetProjectByID(this IRepository<Project> repo, int ID)
+        {
+            var project = repo.All().SingleOrDefault(p => p.ID == ID);
+            if (project == null)
+            {
+                throw new ArgumentException("Invalid project ID");
+            }
+            return project;
+        }
+    }
+}
